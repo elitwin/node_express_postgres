8cb8dee96e04cda964a4173d5f4f54b45033d18f
Eric Litwin <eric.litwin@pnmac.com>
2019-09-13 15:02:20 -0700

    express added with basic route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import 'dotenv/config';
+import express from 'express';
 
-console.log('Hello Node.js project.');
+const app = express();
+
 console.log(process.env.MY_SECRET);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
