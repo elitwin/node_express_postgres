15d46f44e8aeb9cc62a3dcef516ec8cf26ce14e0
Eric Litwin <eric.litwin@pnmac.com>
2019-09-13 14:21:47 -0700

    use eslint:recommended instead of standard

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
-console.log('Hello Node.js project.')
-console.log(process.env.MY_SECRET)
+console.log('Hello Node.js project.');
+console.log(process.env.MY_SECRET);
