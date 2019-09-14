5ddb11b4987952bac9503f69354d4dc1b341f012
Eric Litwin <eric.litwin@pnmac.com>
2019-09-14 13:12:31 -0700

    sample data moved to models

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import uuidv4 from 'uuid/v4';
+import models from './models';
 
 const app = express();
 
@@ -9,7 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  req.me = users[1];
+  req.context = {
+    models,
+    me: models.users[1],
+  };
   next();
 });
 
@@ -26,34 +30,11 @@ app.delete('/', (req, res) => {
   return res.send('Received a DELETE HTTP method');
 });
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
-
 app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+  return res.send(Object.values(req.context.models.users));
 });
 app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
+  return res.send(req.context.models.users[req.params.userId]);
 });
 app.post('/users', (req, res) => {
   return res.send('POST HTTP method on user resource');
@@ -70,10 +51,10 @@ app.delete('/users/:userId', (req, res) => {
 });
 
 app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
+  return res.send(Object.values(req.context.models.messages));
 });
 app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
+  return res.send(req.context.models.messages[req.params.messageId]);
 });
 app.post('/messages', (req, res) => {
   const id = uuidv4();
@@ -82,11 +63,11 @@ app.post('/messages', (req, res) => {
     text: req.body.text,
     userId: req.me.id,
   };
-  messages[id] = message;
+  req.context.models.messages[id] = message;
   return res.send(message);
 });
 app.put('/messages/:messageId', (req, res) => {
-  let message = messages[req.params.messageId];
+  let message = req.context.models.messages[req.params.messageId];
   if (message) {
     message.text = req.body.text;
     return res.send(message);
@@ -99,12 +80,12 @@ app.delete('/messages/:messageId', (req, res) => {
   const {
     [req.params.messageId]: message,
     ...otherMessages
-  } = messages;
-  messages = otherMessages;
+  } = req.context.model.messages;
+  req.context.models.messages = otherMessages;
   return res.send(message);
 });
 app.get('/session', (req, res) => {
-  return res.send(users[req.me.id]);
+  return res.send(req.context.models.users[req.context.me.id]);
 });
 
 app.listen(process.env.PORT, () =>
